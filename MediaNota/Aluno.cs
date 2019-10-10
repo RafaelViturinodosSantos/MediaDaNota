dbea7cfa083516f51fa1caac0545d7bd8ad71507
Rafael <rafa.viturino94@gmail.com>
2019-10-10 15:30:34 -0300

    add new class Program and Aluno

---
+++ MediaNota/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace MediaNota
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Matricula { get; private set; }
+        public double Nota1 { get; private set; }
+        public double Nota2 { get; private set; }
+
+        public Aluno(string nome, int matricula, double nota1, double nota2)
+        {
+            Nome = nome;
+            Matricula = matricula;
+            Nota1 = nota1;
+            Nota2 = nota2;
+        }
+        public double Media()
+        {
+            return (double)(Nota1 + Nota2) / 2.0;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " da matrícula de número " + Matricula + " a média da sua nota foi "
+                + Media().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ MediaNota/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediaNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a;
+
+            Console.WriteLine("Olá prezado aluno !");
+            Console.Write("Informe seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.Write("Informe seu número da matrícula: ");
+            int matricula = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor da primeira nota: ");
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe o valor da segunda nota: ");
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            a = new Aluno(nome, matricula, nota1, nota2);
+
+            Console.WriteLine(a);
+
+
+            if (a.Media() >= 6.0)
+            {
+                Console.WriteLine("Você foi aprovado !");
+            }
+            else
+            {
+                Console.WriteLine("Você foi reprovado !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
